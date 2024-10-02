bde43d272e5d08419a42fb55b9a6408fe0fec800
Daniel Barta <daniel@slateteams.com>
2024-10-02 15:35:59 +0200

    fix: export WebMediaInfo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { WebDemuxer } from "./web-demuxer";
 
-export type { WebAVStream, WebAVPacket } from './types';
+export type { WebAVStream, WebAVPacket, WebMediaInfo } from './types';
 export { AVMediaType, AVLogLevel, AVSeekFlag } from './types';
 export { WebDemuxer };
